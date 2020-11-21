c2f134f97cf6ad96498fe25008bcc9d9a031b118
csortansz <csortanszilard@yahoo.com>
2020-11-21 02:17:56 +0100

    Add projectJson in order to handle when a user wants to retrieve the result of a running operation

--- src/main/java/server/OpenApiWebServer.java
+++ src/main/java/server/OpenApiWebServer.java
@@ -21,12 +21,12 @@ import java.nio.file.*;
 
 public class OpenApiWebServer extends AbstractVerticle {
     HttpServer server;
+    private static final String DIRECTORY_OF_WORKSPACES = "E:\\Egyetem\\GammaWrapper\\Workspaces\\";
 
-    private final static String WORKSPACE = "C:\\WrapperWorkspace";
+    private final static String WORKSPACE = "0fa44362-3467-477d-9164-ecfe5ba2f8fc";
 
     @Override
     public void start(Future<Void> future) {
-        ProcessBuilderUtil processBuilderUtil = new ProcessBuilderUtil();
         OpenAPI3RouterFactory.create(this.vertx, "gamma-wrapper.yaml", ar ->
         {
             OpenAPI3RouterFactory routerFactory = null;
@@ -34,33 +34,35 @@ public class OpenApiWebServer extends AbstractVerticle {
                 routerFactory = ar.result();
                 routerFactory.addHandlerByOperationId("getGeneratedCode", routingContext -> {
                     RequestParameters params = routingContext.get("parsedParameters");
-                    String projectName = params.pathParameter("projectName").getString();
-                    String filePath = params.pathParameter("filePath").getString();
+                   // String projectName = params.pathParameter("projectName").getString();
+                    //String filePath = params.pathParameter("filePath").getString();
                     String resultPath = params.pathParameter("resultPath").getString();
+                    String projectName = "hu.bme.mit.gamma.tests";
 
-
-                    filePath = filePath.replace("_", "\\");
+                    //filePath = filePath.replace("_", "\\");
+                    String filePath = "E:\\Egyetem\\GammaWrapper\\Workspaces\\0fa44362-3467-477d-9164-ecfe5ba2f8fc\\hu.bme.mit.gamma.tests\\model\\SSM\\System\\Mission.ggen";
                     ZipUtils zipUtils = new ZipUtils();
                     try {
-                        processBuilderUtil.generateCode(projectName, WORKSPACE, filePath);
-                    } catch (IOException | InterruptedException e) {
+                        ProcessBuilderUtil.generateCode(projectName, WORKSPACE, filePath);
+                    } catch (IOException e) {
                         e.printStackTrace();
                     }
-                    String resultZipFilePath = zipUtils.getOutputZipFilePath(WORKSPACE, projectName, resultPath);
+                   // String resultZipFilePath = zipUtils.getOutputZipFilePath(WORKSPACE, projectName, resultPath);
                     routingContext
                             .response()// (1)
                             .setStatusCode(200)
                             .putHeader(HttpHeaders.CONTENT_TYPE, "application/zip")
-                            .sendFile(resultZipFilePath);
+                            .end();
                 });
 
                 routerFactory.addHandlerByOperationId("addProject", routingContext -> {
+
                     for (FileUpload f : routingContext.fileUploads()) {
                         try {
                             Path temp = Files.move
-                                    (Paths.get(f.uploadedFileName()),Paths.get(WORKSPACE + "\\" + f.fileName()));
+                                    (Paths.get(f.uploadedFileName()),Paths.get(DIRECTORY_OF_WORKSPACES +WORKSPACE + "\\" + f.fileName()));
                             String projectName = f.fileName().substring(0,f.fileName().lastIndexOf("."));
-                            processBuilderUtil.createEclipseProject(projectName,WORKSPACE);
+                            ProcessBuilderUtil.createEclipseProject(projectName,WORKSPACE,"owner@owner.hu");
                         } catch (IOException | InterruptedException e) {
                             e.printStackTrace();
                         }
--- src/main/java/util/ProcessBuilderUtil.java
+++ src/main/java/util/ProcessBuilderUtil.java
@@ -1,24 +1,87 @@
 package util;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import org.apache.commons.io.FileUtils;
+import org.joda.time.DateTime;
+import org.json.simple.JSONObject;
+
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Date;
+import java.util.UUID;
 
 public class ProcessBuilderUtil {
 
-    public void generateCode(String projectName, String workspace, String filePath) throws IOException, InterruptedException {
-        String directoryOfHeadlessEclipse = "C:\\Eclipse-2020-09-workspace\\eclipse\\eclipse.exe";
-        String commandToExecute = directoryOfHeadlessEclipse + " -consoleLog -data " + workspace + " " + filePath + " " + projectName;
+    private static final String DIRECTORY_OF_WORKSPACES = "E:\\Egyetem\\GammaWrapper\\Workspaces\\";
+    private static final String DIRECTORY_OF_GENERATOR_HEADLESS_ECLIPSE = "E:\\Egyetem\\GammaWrapper\\HeadlessEclipse\\generator\\eclipse.exe";
+    private static final String DIRECTORY_OF_GAMMA_HEADLESS_ECLIPSE = "E:\\Egyetem\\GammaWrapper\\HeadlessEclipse\\gammaapi\\eclipse.exe";
+    private static final String CONSTANT_ARGUMENTS = " -consoleLog -data ";
+    public static final String PROJECT_DESCRIPTOR_JSON = "projectDescriptor.json";
+    public static final String UNDER_OPERATION_PROPERTY = "underOperation";
 
+
+    public static void generateCode(String projectName, String workspace, String filePath) throws IOException {
+        String commandToExecute = DIRECTORY_OF_GAMMA_HEADLESS_ECLIPSE + CONSTANT_ARGUMENTS + DIRECTORY_OF_WORKSPACES + workspace + " " + filePath
+                + " " + DIRECTORY_OF_WORKSPACES + workspace + "\\" + projectName + "\\" + PROJECT_DESCRIPTOR_JSON;
+        updateUnderOperationStatus(projectName, workspace, true);
         Runtime rt = Runtime.getRuntime();
-        Process pr = rt.exec(commandToExecute);
-        pr.waitFor();
+        rt.exec(commandToExecute);
+
     }
 
-    public void createEclipseProject(String projectName, String workspace) throws IOException, InterruptedException {
-        String directoryOfHeadlessEclipse = "C:\\Eclipse-2020-09-workspace\\generator\\eclipse.exe";
-        String commandToExecute = directoryOfHeadlessEclipse + " -consoleLog -data " + workspace + " "+ projectName;
+    private static void updateUnderOperationStatus(String projectName, String workspace, Boolean status) throws IOException {
+        File jsonFile = new File(DIRECTORY_OF_WORKSPACES + workspace + "\\" + projectName + "\\" + PROJECT_DESCRIPTOR_JSON);
+        String jsonString = FileUtils.readFileToString(jsonFile);
+        JsonElement jElement = new JsonParser().parse(jsonString);
+        JsonObject jObject = jElement.getAsJsonObject();
+        jObject.remove(UNDER_OPERATION_PROPERTY);
+        jObject.addProperty(UNDER_OPERATION_PROPERTY, status);
+
+        Gson gson = new Gson();
+        String resultingJson = gson.toJson(jElement);
+        FileUtils.writeStringToFile(jsonFile, resultingJson);
+
+    }
+
+    private static void createProjectJSONFile(String workspace, String projectName, String ownerContact) {
+        JSONObject jsonObject = new JSONObject();
+        Date today = new Date();
+        DateTime dtOrg = new DateTime(today);
+        DateTime expirationDate = dtOrg.plusDays(30);
+
+        jsonObject.put("projectName", projectName);
+        jsonObject.put("owner", ownerContact);
+        jsonObject.put("creationDate", today.getTime());
+        jsonObject.put("expirationDate", expirationDate.toDate().getTime());
+        jsonObject.put(UNDER_OPERATION_PROPERTY, false);
+
+        try {
+            FileWriter file = new FileWriter(DIRECTORY_OF_WORKSPACES + workspace + "\\" + projectName + "\\" + PROJECT_DESCRIPTOR_JSON);
+            file.write(jsonObject.toJSONString());
+            file.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void createEclipseProject(String projectName, String workspace, String ownerContact) throws IOException, InterruptedException {
+        String commandToExecute = DIRECTORY_OF_GENERATOR_HEADLESS_ECLIPSE + CONSTANT_ARGUMENTS + DIRECTORY_OF_WORKSPACES + workspace + " " + projectName;
         Runtime rt = Runtime.getRuntime();
         Process pr = rt.exec(commandToExecute);
         pr.waitFor();
+        createProjectJSONFile(workspace, projectName, ownerContact);
+    }
 
+    public static String createWorkspaceForUser() throws IOException, InterruptedException {
+        String workspace = String.valueOf(UUID.randomUUID());
+        String commandToExecute = DIRECTORY_OF_GENERATOR_HEADLESS_ECLIPSE + CONSTANT_ARGUMENTS + DIRECTORY_OF_WORKSPACES + workspace;
+        Runtime rt = Runtime.getRuntime();
+        Process pr = rt.exec(commandToExecute);
+        pr.waitFor();
+        return workspace;
     }
 }
